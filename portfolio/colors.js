9e043ab97c9f7e3644150f49a3d250a5748ef162
Gabriela Shirley <gabrielashirleyh@gmail.com>
2020-07-09 12:01:18 -0700

    Deploy gatsby

---
+++ portfolio/colors.js
@@ -0,0 +1,14 @@
+module.exports = {
+  background: '#FFFFFF',
+  backgroundDark: '#fdfdc4',
+
+  text: '#333333',
+
+  primary: '#ff9292',
+  primaryLight: '#ffc5c5',
+  primaryDark: '#ffabab',
+
+  secondary: '#80ffd0',
+  secondaryLight: '#b3ffe3',
+  secondaryDark: '#ff9292',
+};
